a4c27138f1488c9876bd70b9bdd0be3238877343
Winson Heng <winsonheng@hotmail.com>
2026-01-02 14:45:53 +0800

    chore: update config

--- hefty_chest/lib/core/config.dart
+++ hefty_chest/lib/core/config.dart
@@ -4,7 +4,8 @@ class AppConfig {
   AppConfig._();
   // static const String backendUrl ='https://heft-backend.onrender.com';
   /// Backend API base URL
-  static const String backendUrl = kReleaseMode ? 'https://heft-backend.onrender.com' : 'http://localhost:8080' ;
+  
+  static const String backendUrl = kReleaseMode ? 'https://heft-751339253558.asia-southeast1.run.app' : 'http://localhost:8080' ;
 
   /// Default rest timer duration in seconds
   static const int defaultRestTimerSeconds = 90;
